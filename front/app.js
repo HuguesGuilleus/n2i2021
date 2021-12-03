0d114b7804d6983e731e6a1fe709e6473c2d99e2
hugues <ghugues@netc.fr>
2021-12-03 06:23:01 +0100

    Add a menu in the about page

---
+++ front/app.js
@@ -0,0 +1,52 @@
+(function () {
+	function main() {
+		loadmenu();
+	}
+	document.readyState == 'loading' ? document.addEventListener('DOMContentLoaded', main, {
+		once: true
+	}) : main();
+}());
+
+async function loadmenu() {
+	document.body.insertAdjacentHTML('beforeend',
+		await fetch("/menu.html").then(rep => rep.text())
+	);
+	let menuGroup = document.getElementById("menu-group");
+	let bodyList = null;
+
+	function menuSwitch() {
+		if (bodyList) {
+			for (let e of bodyList) {
+				document.body.append(e);
+			}
+			bodyList = null;
+			menuGroup.hidden = true;
+		} else {
+			bodyList = Array.from(document.body.children);
+			bodyList.filter(e => e !== menuGroup).forEach(e => e.remove());
+			menuGroup.hidden = false;
+		}
+	}
+	document.getElementById("menu-button").addEventListener("click", menuSwitch);
+	document.addEventListener("keydown", menuSwitch);
+
+	let user = {
+		isconnected: false,
+		isadmin: false,
+	};
+	user.isconnected = await fetch("/isconnected").then(rep => rep.json());
+	if (user.isconnected) {
+		user.isadmin = await fetch("/isadmin").then(rep => rep.json());
+	}
+	if (user.isconnected) {
+		document.getElementById("menu-login").hidden = true;
+		if (!user.isadmin) {
+			document.getElementById("menu-waitvalidation").hidden = true;
+			document.getElementById("menu-historic").hidden = true;
+		}
+	} else {
+		document.getElementById("menu-add").hidden = true;
+		document.getElementById("menu-waitvalidation").hidden = true;
+		document.getElementById("menu-historic").hidden = true;
+	}
+}

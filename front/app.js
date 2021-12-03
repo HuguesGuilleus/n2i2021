176f2f1392774f2159a1d69ccba62f8381da31fc
hugues <ghugues@netc.fr>
2021-12-03 07:49:03 +0100

    Load stats with js in main page

--- front/app.js
+++ front/app.js
@@ -1,6 +1,9 @@
 (function () {
 	function main() {
 		loadmenu();
+		if (location.pathname === "/") {
+			loadStats();
+		}
 	}
 	document.readyState == 'loading' ? document.addEventListener('DOMContentLoaded', main, {
 		once: true
@@ -53,5 +56,27 @@ async function loadmenu() {
 		document.getElementById("menu-add").hidden = true;
 		document.getElementById("menu-waitvalidation").hidden = true;
 		document.getElementById("menu-historic").hidden = true;
+	}
+}
+
+async function loadStats() {
+	const reponse = await fetch("stats.json");
+	const stats = await reponse.json();
+
+	const statsGroup = document.getElementById("stats-group");
+	for (const stat of stats) {
+		const li = document.createElement("li");
+		li.classList.add("stats-item");
+		statsGroup.append(li);
+
+		const statNb = document.createElement("span");
+		statNb.classList.add("stats-nb");
+		statNb.innerText = stat.nb;
+		li.append(statNb);
+
+		const statDesc = document.createElement("span");
+		statNb.classList.add("stats-desc");
+		statDesc.innerText = stat.text;
+		li.append(statDesc);
 	}
 }
